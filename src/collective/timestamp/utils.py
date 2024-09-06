d1386c6a08ae902decd9708befc14fb132e048b8
Laurent Lasudry <lasudry@gmail.com>
2024-09-06 15:01:19 +0200

    Harmonize code

--- src/collective/timestamp/utils.py
+++ src/collective/timestamp/utils.py
@@ -9,6 +9,11 @@ from rfc3161ng import TimeStampResp
 import pytz
 
 
+def localize_utc_date(date):
+    tzinfo = pytz.timezone("UTC")
+    return tzinfo.localize(date)
+
+
 def get_timestamp(file_content):
     service_url = api.portal.get_registry_record(
         "timestamping_service_url",
@@ -25,18 +30,15 @@ def get_timestamp(file_content):
         return_tsr=True,
     )
     timestamp_token = tsr.time_stamp_token
-    tzinfo = pytz.timezone("UTC")
     timestamp_date = get_timestamp_date(timestamp_token)
     return {
         "tsr": encoder.encode(tsr),
-        "timestamp_date": tzinfo.localize(timestamp_date),
+        "timestamp_date": localize_utc_date(timestamp_date),
     }
 
 
 def get_timestamp_date_from_tsr(tsr_data):
     tsr, _ = decoder.decode(tsr_data, asn1Spec=TimeStampResp())
-    timestamp_token = tsr['timeStampToken']
+    timestamp_token = tsr.time_stamp_token
     timestamp_date = get_timestamp_date(timestamp_token)
-    tzinfo = pytz.timezone("UTC")
-    timestamp_date = tzinfo.localize(timestamp_date)
-    return timestamp_date
+    return localize_utc_date(timestamp_date)
