b940fa1de537da5704b152ddbe3096b0748cc56b
Laurent Lasudry <lasudry@gmail.com>
2024-09-11 12:51:59 +0200

    Define viewlet template in class to ease tests

--- src/collective/timestamp/browser/viewlet.py
+++ src/collective/timestamp/browser/viewlet.py
@@ -2,9 +2,11 @@
 
 from collective.timestamp.interfaces import ITimeStamper
 from plone.app.layout.viewlets import common
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class TimestampViewlet(common.ViewletBase):
+    index = ViewPageTemplateFile("templates/viewlet.pt")
 
     def available(self):
         handler = ITimeStamper(self.context)
